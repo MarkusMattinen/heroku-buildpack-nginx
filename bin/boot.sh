052a91212e3089d4bcca3ec3e9dfac1e79cd45ca
Markus Mattinen <markus.mattinen@vincit.fi>
2015-04-24 14:06:57 +0300

    Log to stdout

--- bin/boot.sh
+++ bin/boot.sh
@@ -6,6 +6,7 @@ erb $HOME/config/nginx.conf.erb > $HOME/config/nginx.conf
 mkdir -p $HOME/logs/nginx
 touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
-(tail -f -n 0 $HOME/logs/nginx/*.log &)
+chown -R root:root $HOME/config $HOME/logs
+chmod -R g-g,o-o $HOME/config $HOME/logs
 
 exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
